149821ac976136a08307eab817136753e7ddb89a
adi-ray <adityadns03@gmail.com>
2025-05-26 02:58:38 +0530

    Add create ticket API

--- src/index.js
+++ src/index.js
@@ -2,21 +2,27 @@ const express = require("express");
 const bodyParser = require("body-parser");
 
 const { PORT } = require("./config/server-config");
+const { createChannel, subscribeMessage } = require("./utils/messageQueue");
+const { REMINDER_BINDING_KEY } = require("./config/server-config");
 
 const TicketController = require("./controllers/ticket-controller");
+const EmailService = require("./services/email-service");
 
 const jobs = require("./utils/job");
 
-const setupAndStartServer = () => {
+const setupAndStartServer = async () => {
   const app = express();
   app.use(bodyParser.json());
   app.use(bodyParser.urlencoded({ extended: true }));
+
+  const channel = await createChannel();
+  subscribeMessage(channel, EmailService.subscribeEvents, REMINDER_BINDING_KEY);
 
   app.post("/api/v1/tickets", TicketController.create);
 
   app.listen(PORT, () => {
     console.log(`Server started at port ${PORT}`);
-    jobs();
+    // jobs();
   });
 };
 
